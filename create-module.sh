4d4bebd4484a7941ef394cf028adb406c28207cc
mongmeo-dev <mongmeo-dev@gmail.com>
2023-10-03 16:54:56 +0900

    create-module 스크립트 작성

---
+++ create-module.sh
@@ -0,0 +1,25 @@
+if [[ ${1} == '' ]]; then
+  echo 'Module name not provided'
+  exit 1
+fi
+
+
+nest g mo ${1}
+
+mkdir src/${1}/queries
+mkdir src/${1}/queries/handlers
+
+mkdir src/${1}/commands
+mkdir src/${1}/commands/handlers
+
+mkdir src/${1}/models
+
+upper=`echo ${1:0:1} | tr  '[a-z]' '[A-Z]'`${1:1}
+mkdir src/${1}/infra
+mkdir src/${1}/infra/web
+echo "import { Controller } from '@nestjs/common';
+
+@Controller('/api/v1/${1}')
+export class ${upper}Controller {}" > src/${1}/infra/web/${1}.controller.ts
+
+echo 'Please add controller in module.'
