1d7f22c456ad74f3ece36cd238813bc436fcb129
mongmeo-dev <mongmeo.dev@gmail.com>
2023-10-10 06:18:30 +0900

    add openapi generator

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { HttpStatus } from '@nestjs/common';
 
 let server: Handler;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap(generateDocs: boolean = false): Promise<Handler> {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: ['http://localhost:3000', 'https://api.arthive.dev'],
@@ -17,13 +19,21 @@ async function bootstrap(): Promise<Handler> {
   });
   await app.init();
 
-  const config = new DocumentBuilder()
-    .setTitle('Arthive! API docs')
-    .setVersion('1.0')
-    .addServer('https://api.arthive.dev')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('_api-docs', app, document);
+  if (generateDocs) {
+    const config = new DocumentBuilder()
+      .setTitle('Arthive! API docs')
+      .setVersion('1.0')
+      .addServer('https://api.arthive.dev')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    fs.writeFileSync('./openapi.json', document.openapi);
+    return async (event: any, context: Context, callback: Callback) => {
+      return {
+        body: 'ok',
+        statusCode: HttpStatus.OK,
+      };
+    };
+  }
 
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
@@ -32,4 +42,8 @@ async function bootstrap(): Promise<Handler> {
 export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
   server = server ?? (await bootstrap());
   return server(event, context, callback);
+};
+
+export const generateApiDocs: Handler = async (event: any, context: Context, callback: Callback) => {
+  return await bootstrap(true);
 };
